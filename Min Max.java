11ffdd03a094e4fd6ea0b52cb27a14272fcc914a
Anupog <anupongsutlek022@gmail.com>
2022-10-31 21:11:49 +0700

    java0002

---
+++ Min Max.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        System.out.println(arr[0]);
+        System.out.println(arr[arr.length - 1]);
+
+    }
+}
